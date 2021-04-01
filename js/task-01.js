25a13fafa60af1b1a539e88d99e72f84a5a13f75
inna-dmytrenko <73888831+inna-dmytrenko@users.noreply.github.com>
2021-04-01 12:53:32 +0300

    task_03

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,44 @@
-const firstMenuItem = document.querySelector('.item')
-console.log(firstMenuItem)
+// Напиши скрипт, який виконає наступні операції.
+
+// Порахує і виведе в консоль кількість категорій 
+// в ul#categories, тобто елементів li.item.Вийде 
+// 'У списку 3 категорії.'.
+
+// Для кожного елемента li.item в списку ul#categories,
+// знайде і виведе в консоль текст заголовка елемента(тега h2)
+// і кількість елементів в категорії
+// (всіх вкладених в нього елементів li).
+
+// Наприклад, для першої категорії вийде:
+
+// Категорія: Тварини
+// Кількість елементів: 4
+
+// const firstMenuItem = document.querySelectorAll('.item')
+// console.log(`У списку ${firstMenuItem.length} категорії`)
+// const firstCategories = document.querySelectorAll('h2')
+// const title = firstCategories[0].textContent;
+// console.log(title)
+// const secondMenuItem = document.querySelectorAll('title')
+// const message = `Кількість елементів: ${secondMenuItem.length}`
+// console.log(message)
+
+// const paragraphs = document.querySelectorAll('h2');
+// // paragraphs[0] is the first <p> element
+// // paragraphs[1] is the second <p> element, etc.
+// console.log(paragraphs[0].nodeName);
+
+const categoriesEl = document.querySelectorAll("li.item")
+console.log(`В списке ${categoriesEl.length} категории`);
+// const items = document.querySelectorAll('.item');
+// Array.prototype.forEach.call(items, (element) => {
+// const title = element.querySelector('h2').innerHTML;
+// const itemsLength = element.querySelectorAll('li').length;
+// console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
+// });
+const ul = Array.from(document.querySelector('#categories').children);
+for (let elem of ul) {
+    let category = elem.firstElementChild.textContent;   
+    let quantityElem = elem.lastElementChild.children.length;
+    console.log(`Категория: ${category} \nКоличество элементов: ${quantityElem}`);
+};
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Зелень',
   'Приправи',
 ];
+
+// Напиши скрипт, который для каждого элемента массива ingredients
+// создаст отдельный li, после чего вставит все li за одну операцию
+// в список ul.ingredients.Для создания DOM - узлов
+// используй document.createElement().
+const ulEl = document.querySelector('ul#ingredients')
+for (const i of ingredients)
+{
+  
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.textContent = i;
+  
+  ulEl.append(ingredientsEl)
+}
+console.log(ulEl)
+
